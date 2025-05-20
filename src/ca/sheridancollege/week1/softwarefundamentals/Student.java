04a0d32d86bd73e9e3172ea0b54fa1d34e7294ea
tumaalex <tumanan@sheridancollege.ca>
2025-05-20 12:47:22 -0400

    Added age

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -15,12 +15,21 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student 
 {
     private String name;
+    private int age;
 
     /**
      * @return the name
      */
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     /**
