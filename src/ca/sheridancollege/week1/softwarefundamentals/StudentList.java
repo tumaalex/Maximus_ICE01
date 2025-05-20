0bd1d7b60590bb78681185729d1e42e9b0111c81
tumaalex <tumanan@sheridancollege.ca>
2025-05-20 12:56:54 -0400

    Update StudentList.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -17,7 +17,7 @@ public class StudentList {
     {
        Student [] studentList = new Student[1];//change size before running
        Student s1 = new Student();
-       s1.setName("Liz Dancy");
+       s1.setName("Alex Tumanan");
        studentList[0]=s1;
        //fill in 4 student names before running
        for(Student s: studentList)
